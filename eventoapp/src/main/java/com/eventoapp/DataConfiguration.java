ac1cf8285bf7b783ec184f8c21c239b2f843f292
Wagner <wagnerjps@gmail.com>
2020-02-17 12:58:13 -0300

    correção postgresql 10 Uri

--- eventoapp/src/main/java/com/eventoapp/DataConfiguration.java
+++ eventoapp/src/main/java/com/eventoapp/DataConfiguration.java
@@ -50,10 +50,10 @@ public class DataConfiguration {
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        String driver = "org.postgresql.Driver";
+       //String driver = "org.postgresql.Driver";
         
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName(driver);
+        //basicDataSource.setDriverClassName(driver);
         basicDataSource.setUrl(dbUrl);
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
