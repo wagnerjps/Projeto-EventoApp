f64cd5aa9813fbb2f7687bff6f8a00eb15eac1e9
Wagner <wagnerjps@gmail.com>
2020-02-17 16:13:12 -0300

    nova configuração

--- eventoapp/src/main/java/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/models/Evento.java
@@ -3,6 +3,7 @@ package com.eventoapp.models;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -25,7 +26,7 @@ public class Evento implements Serializable{
 	@GeneratedValue(strategy= GenerationType.AUTO) 		//Forçar a geração automática do ID
 	private Long codigo;								//Campo que será o ID da tabela
 
-	@OneToMany
+	@OneToMany( mappedBy="evento", cascade=CascadeType.ALL, orphanRemoval=true)
 	private List<Convidado> convidados;
 	
 	@NotEmpty
